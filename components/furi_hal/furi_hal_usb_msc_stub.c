440f6a1d3cc33fffabaf5cbb37f15122ee9b9d88
Jeka725 <zeka19250@gmail.com>
2026-09-24 14:57:01 +0700

    Fix: provide USB MSC stubs without SD

---
+++ components/furi_hal/furi_hal_usb_msc_stub.c
@@ -0,0 +1,16 @@
+#include "furi_hal_usb_msc.h"
+
+bool furi_hal_usb_msc_start(void) {
+    return false;
+}
+
+void furi_hal_usb_msc_stop(void) {
+}
+
+bool furi_hal_usb_msc_is_active(void) {
+    return false;
+}
+
+bool furi_hal_usb_msc_is_removal_locked(void) {
+    return false;
+}
