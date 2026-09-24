8c11a89ec7042b06fdeba9800daba13366f2dce4
Jeka725 <zeka19250@gmail.com>
2026-09-25 05:16:56 +0700

    Reduce animation serial output to one diagnostic line

--- components/desktop/animations/views/bubble_animation_view.c
+++ components/desktop/animations/views/bubble_animation_view.c
@@ -239,12 +239,7 @@ static void bubble_animation_timer_callback(void* context) {
     if(!model->freeze_frame && !activate) {
         bubble_animation_next_frame(model);
         if(model->current) {
-            FURI_LOG_I(
-                "DolphinAnim",
-                "Drawing frame %u/%u",
-                (unsigned)model->current_frame + 1,
-                (unsigned)model->current->passive_frames +
-                    (unsigned)model->current->active_frames);
+            
         }
     }
 
